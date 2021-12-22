05b862aa79b0d28397af7f55f6ba89a64f4c2e20
serifinal <84276150+serifinal@users.noreply.github.com>
2021-12-22 18:02:11 +0700

    upload
    upload giao diện user

--- Code/user/categories.php
+++ Code/user/categories.php
@@ -8,51 +8,48 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
   integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
   <title>Document</title>
 </head>
 
 <body>
-  <!-- navbar -->
-  <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light main-navbar">
+    <header>
+      <!-- navbar -->
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Logo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="logIn.html" tabindex="-1" aria-disabled="true">Đăng nhập</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="signup.html" tabindex="-1" aria-disabled="true">Đăng kí</a>
-            </li>
-          </ul>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light main-navbar">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link" href="user.php" tabindex="-1" aria-disabled="true">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="categories.php" tabindex="-1" aria-disabled="true">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="userInfo.php" tabindex="-1" aria-disabled="true">User info</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
         </div>
       </div>
-    </nav>
-  </div>
+  </header>
 
   <main>
-    <!-- Slider -->
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="assets/img/slider.jpg" class="d-block w-100" alt="slider">
-            </div>
-        </div>
-    </div>
-
     <!-- Hot tours -->
     <section class="container mt-5">
        <h3 style="color: red;">Các tour đang hot <i class="fas fa-fire-alt"></i></h3>
         <div class="popular-post row">
             <div class="col-md-4 mt-3">
                 <div class="card" style="width: 100%;">
-                    <img src="assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Tour La Khê -Đống Đa</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
@@ -64,7 +61,7 @@
 
             <div class="col-md-4 mt-3">
                 <div class="card" style="width: 100%;">
-                    <img src="assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Tour La Khê -Đống Đa</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
@@ -76,7 +73,7 @@
 
             <div class="col-md-4 mt-3">
                 <div class="card" style="width: 100%;">
-                    <img src="assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
+                    <img src="../assets/img/du-lich-hoi-an.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Tour La Khê -Đống Đa</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
@@ -95,33 +92,30 @@
             <div class="row mt-3">
                 <div class="col-md-4">
                     <div class="card"">
-                        <img src="assets/img/ha-noi-mien-bac.jpg" class="card-img-top" alt="...">
+                        <img src="../assets/img/ha-noi-mien-bac.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Miền Bắc</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="user/categories.php" class="btn btn-primary">Xem Tour</a>
                         </div>
                     </div>
                 </div>
     
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="assets/img/ha-noi-mien-bac.jpg" class="card-img-top img-fluid" alt="...">
+                        <img src="../assets/img/ha-noi-mien-bac.jpg" class="card-img-top img-fluid" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Miền Trung</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="user/categories.php" class="btn btn-primary">Xem Tour</a>
                         </div>
                     </div>
                 </div>
     
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="assets/img/ha-noi-mien-bac.jpg" class="card-img-top" alt="...">
+                        <img src="../assets/img/ha-noi-mien-bac.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Miền Nam</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="user/categories.php" class="btn btn-primary">Xem Tour</a>
                         </div>
                     </div>
                 </div>
@@ -138,7 +132,7 @@
                 <div class="card">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -155,44 +149,7 @@
                 <div class="card">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Đặt ngay</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="row mt-3">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Đặt ngay</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -212,7 +169,7 @@
                 <div class="card">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -229,7 +186,44 @@
                 <div class="card">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Đặt ngay</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Đặt ngay</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../assets/img/mien-trung.jpg" alt="" class="img-fluid">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -245,6 +239,7 @@
         </div>
 
     </section>
+      
 
   </main>
 
