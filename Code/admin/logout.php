7a2ee2ad9e12dd6bf1321c4fe95cc647ef03e248
duongcongit <duongcongit@gmail.com>
2021-12-30 12:39:05 +0700

    delete

--- Code/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include('../config/constants.php');
-    // Kiểm tra và đăng xuất
-    if(isset($_SESSION['adminAccount'])){
-        unset($_SESSION['adminAccount']);
-        header("location:" . SITEURL . "admin/login.php");
-    }
-
-?>
--- Code/admin/partials/login-check.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!isset($_SESSION['adminAccount'])) {
-    header("location:" . SITEURL . "admin/login.php");
-}
-
-?>
